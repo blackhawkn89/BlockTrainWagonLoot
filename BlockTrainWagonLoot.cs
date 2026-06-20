865a28053df4d0581c548e5d39490ed56796c3ea
MrHawk <daniel@ljokjell.com>
2026-06-20 11:08:19 +0200

    Plugin and logo upload

---
+++ BlockTrainWagonLoot.cs
@@ -0,0 +1,58 @@
+using Oxide.Plugins;
+
+namespace Oxide.Plugins
+{
+    [Info("BlockTrainWagonLoot", "MrHawk", "1.0.0")]
+    [Description("Removes the unlootable/bugged wagons")]
+
+    public class BlockTrainWagonUnloadableLoot : RustPlugin
+    {
+        private void OnServerInitialized()
+        {
+            foreach (var entity in BaseNetworkable.serverEntities)
+            {
+                if (
+                    entity != null &&
+                    entity.ShortPrefabName.Contains(
+                        "trainwagonunloadableloot"
+                    )
+                )
+                {
+                    Puts(
+                        $"Removing existing: {entity.ShortPrefabName}"
+                    );
+
+                    entity.Kill();
+                }
+            }
+        }
+
+        private void OnEntitySpawned(
+            BaseNetworkable entity
+        )
+        {
+            if (
+                entity != null &&
+                entity.ShortPrefabName.Contains(
+                    "trainwagonunloadableloot"
+                )
+            )
+            {
+                Puts(
+                    $"Blocking spawn: {entity.ShortPrefabName}"
+                );
+
+                timer.Once(0.1f, () =>
+                {
+                    if (
+                        entity != null &&
+                        !entity.IsDestroyed
+                    )
+                    {
+                        entity.Kill();
+                    }
+                });
+            }
+        }
+    }
+}
